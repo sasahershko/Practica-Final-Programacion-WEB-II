9587b077d16bde9d32fca0d8f4369cd827dd020c
sasahershko <sarayhershko@gmail.com>
2025-04-08 22:22:46 +0200

    Update deliveryNote.js

--- models/deliveryNote.js
+++ models/deliveryNote.js
@@ -1,118 +1,116 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-
 const addressSchema = new Schema(
-  {
-    street: String,
-    number: Number,
-    postal: Number,
-    city: String,
-    province: String
-  },
-  { _id: false }
+    {
+        street: String,
+        number: Number,
+        postal: Number,
+        city: String,
+        province: String
+    },
+    { _id: false }
 );
 
+
 const companySchema = new Schema(
-  {
-    name: String,
-    cif: String,
-    street: String,
-    number: Number,
-    postal: Number,
-    city: String,
-    province: String
-  },
-  { _id: false }
+    {
+        name: String,
+        cif: String,
+        street: String,
+        number: Number,
+        postal: Number,
+        city: String,
+        province: String
+    },
+    { _id: false }
 );
 
 // si se quisiera duplicar info completa
 const embeddedClientSchema = new Schema(
-  {
-    name: String,
-    cif: String,
-    address: addressSchema
-  },
-  { _id: false }
+    {
+        name: String,
+        cif: String,
+        address: addressSchema
+    },
+    { _id: false }
 );
 
 const deliveryNoteSchema = new Schema(
-  {
-    // Referencias a User, Client, Project (para relacionarlo con tu BD)
-    userId: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
-    },
-    clientId: {
-      type: Schema.Types.ObjectId,
-      ref: 'Client'
-    },
-    projectId: {
-      type: Schema.Types.ObjectId,
-      ref: 'Project'
-    },
+    {
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        clientId: {
+            type: Schema.Types.ObjectId,
+            ref: 'Client'
+        },
+        projectId: {
+            type: Schema.Types.ObjectId,
+            ref: 'Project'
+        },
 
-    company: companySchema,
-    client: embeddedClientSchema,
+        company: companySchema,
+        client: embeddedClientSchema,
 
-    // Información general del albarán
-    name: {
-      type: String,
-      default: '' // Ej: nombre del operario o responsable
-    },
-    date: {
-      type: Date,
-      default: Date.now // o la fecha que se especifique
-    },
-    project: {
-      type: String, // ej: "0002"
-      default: ''
-    },
-    description: {
-      type: String,
-      default: ''
-    },
+        name: {
+            type: String,
+            default: ''
+        },
+        date: {
+            type: Date,
+            default: Date.now // o la fecha que se especifique
+        },
+        project: {
+            type: String, // ej: "0002"
+            default: ''
+        },
+        description: {
+            type: String,
+            default: ''
+        },
 
-    format: {
-      type: String,
-      enum: ['hours', 'materials'],
-      default: 'hours'
-    },
+        format: {
+            type: String,
+            enum: ['hours', 'materials'],
+            default: 'hours'
+        },
 
-    // Campos para "hours" y "workers"
-    hours: {
-      type: Number,
-      default: 0
-    },
-    workers: {
-      type: [String], // o subdocumentos si necesitas más info
-      default: []
-    },
+        hours: {
+            type: Number,
+            default: 0
+        },
+        workers: {
+            type: [String], // o subdocumentos si necesitas más info
+            default: []
+        },
 
-    photo: {
-      type: String
-    },
+        photo: {
+            type: String
+        },
 
-    sign: {
-      type: String, // ruta o URL de la firma
-      default: false
-    },
+        sign: {
+            type: String, // ruta o URL de la firma
+            default: false
+        },
 
-    // para saber si está pendiente, firmado, etc
-    pending: {
-      type: Boolean,
-      default: true
-    },
 
-    archived: {
-      type: Boolean,
-      default: false
+        // para saber si está pendiente, firmado, etc
+        pending: {
+            type: Boolean,
+            default: true
+        },
+
+        archived: {
+            type: Boolean,
+            default: false
+        }
+    },
+    {
+        timestamps: true // createdAt, updatedAt
     }
-  },
-  {
-    timestamps: true // createdAt, updatedAt
-  }
 );
 
 module.exports = mongoose.model('DeliveryNote', deliveryNoteSchema);
